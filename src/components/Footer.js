185548b524ff40307f90c48686e77860ae3b62c1
Tcodingg <32871055+Tcodingg@users.noreply.github.com>
2021-04-19 14:52:46 -0400

    hide the footer when the url is /cart

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,15 @@ import {useLocation} from 'react-router-dom'
 
 
 export default function Footer() {
-    
+
+    //hide footer in the cart url
+    const [hideFooter, setHideFooter] = useState(false);
+    let location = useLocation();
+    useEffect(()=>{
+        location.pathname === '/cart'? setHideFooter(true): setHideFooter(false);
+    },[location]) ;
+
+
     return (
         <footer style={{display: hideFooter? 'none': 'block'}} className="footer section bd-container">
             <div className="footer_container bd-grid">
@@ -17,7 +25,6 @@ export default function Footer() {
                         <a href="/#" className="social"><RiTwitterFill /></a>
                         <a href="/#" className="social"><RiInstagramFill/></a>
                     </div>
-
                 </div>
                 <div className="footer_content">
                     <h3 className="footer_title">Services</h3>
@@ -27,7 +34,6 @@ export default function Footer() {
                         <li><a href="/#" className="footer_link">Customer Service</a></li>
                     </ul>
                 </div>
-
                 <div className="footer_content">
                     <h3 className="contact footer_title ">Contact Us</h3>
                     <ul>
@@ -38,7 +44,6 @@ export default function Footer() {
                 </div>
             </div>
             <p className="footer_copy">&#169; Breakfast Club 2021. All rights reserved.</p>
-
         </footer>
     )
 }
