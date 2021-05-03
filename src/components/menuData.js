5e5fe5bf0b394559cb6678627b163283d9626153
Tcodingg <32871055+Tcodingg@users.noreply.github.com>
2021-05-03 13:50:01 -0400

    change prices for producgts

--- src/components/menuData.js
+++ src/components/menuData.js
@@ -1,45 +1,45 @@
-import baconBreakfast from '../images/Bacon Breakfast.png'
-import BagelBELT from '../images/Bagel BELT.png'
-import EggandCheese from '../images/Egg and Cheese.png'
-import FarmerSandwich from '../images/Farmer Sandwich.png'
-import FarmerWrap from '../images/Grilled Wrap.png'
-import SausageandBacon from '../images/Sausage and Bacon.png'
-import SausageBreakfast from '../images/Sausage Breakfast.png'
+import baconBreakfast from "../images/Bacon Breakfast.png";
+import BagelBELT from "../images/Bagel BELT.png";
+import EggandCheese from "../images/Egg and Cheese.png";
+import FarmerSandwich from "../images/Farmer Sandwich.png";
+import FarmerWrap from "../images/Grilled Wrap.png";
+import SausageandBacon from "../images/Sausage and Bacon.png";
+import SausageBreakfast from "../images/Sausage Breakfast.png";
 export const menuData = [
-    {
-        id: 1, 
-        image: baconBreakfast,
-        title: 'Bacon Breakfast', 
-        price:'$3.99',
-    },
-    {
-        id: 2, 
-        image: BagelBELT,
-        title: 'Bagle BELT', 
-        price:'$3.99',
-    },
-    {
-        id: 3, 
-        image: EggandCheese,
-        title: 'Egg & Cheese', 
-        price:'$3.99',
-    },
-    {
-        id: 4, 
-        image: FarmerSandwich,
-        title: 'Farmer Sandwich', 
-        price:'$3.99',
-    },
-    {
-        id: 5, 
-        image: FarmerWrap,
-        title: 'Farmer Wrap', 
-        price:'$3.99',
-    },
-    {
-        id: 6, 
-        image: SausageandBacon,
-        title: 'Sausage & Bacon', 
-        price:'$3.99',
-    },
-]
+  {
+    id: 1,
+    image: baconBreakfast,
+    title: "Bacon Breakfast",
+    price: "$3.99",
+  },
+  {
+    id: 2,
+    image: BagelBELT,
+    title: "Bagle BELT",
+    price: "$6.49",
+  },
+  {
+    id: 3,
+    image: EggandCheese,
+    title: "Egg & Cheese",
+    price: "$2.99",
+  },
+  {
+    id: 4,
+    image: FarmerSandwich,
+    title: "Farmer Sandwich",
+    price: "$4.29",
+  },
+  {
+    id: 5,
+    image: FarmerWrap,
+    title: "Farmer Wrap",
+    price: "$5.99",
+  },
+  {
+    id: 6,
+    image: SausageandBacon,
+    title: "Sausage & Bacon",
+    price: "$3.99",
+  },
+];
