ddbbb392c702d741f8a07929925e088687919917
DePrincio <princetlive@yahoo.com>
2021-05-11 11:45:37 -0400

    fix deployment issue

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,16 +1,15 @@
-import React from 'react'
-import Home from './Home'
-import Services from './Services'
-import Menu from './Menu'
-import Footer from './Footer'
+import React from 'react';
+import Home from './Home';
+import Services from './Services';
+import Menu from './Menu';
+import Footer from './Footer';
 
 export default function LandingPage() {
-    return (
-        <div>
-            <Home />
-            <Services />
-            <Menu />
-         
-        </div>
-    )
+	return (
+		<div>
+			<Home />
+			<Services />
+			<Menu />
+		</div>
+	);
 }
